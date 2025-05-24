e4eb00144ccac643f9563b5ee02581ebd1d74d27
Catalin-Marian Tache <catalinmariantache@gmail.com>
2025-05-24 15:46:18 +0300

    Feature/auth: implement register(), attempt(), userExists() in AuthService

--- app/Domain/Service/AuthService.php
+++ app/Domain/Service/AuthService.php
@@ -19,6 +19,13 @@ class AuthService
         // TODO: make sure password is not stored in plain, and proper PHP functions are used for that
 
         // TODO: here is a sample code to start with
+
+        if ($this->userExists($username)) {
+            throw new \RuntimeException("Username '{$username}' already taken");
+        }
+
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
         $user = new User(null, $username, $password, new \DateTimeImmutable());
         $this->users->save($user);
 
@@ -27,10 +34,18 @@ class AuthService
 
     public function attempt(string $username, string $password): bool
     {
-        // TODO: implement this for authenticating the user
-        // TODO: make sur ethe user exists and the password matches
-        // TODO: don't forget to store in session user data needed afterwards
+        $user = $this->users->findByUsername($username);
+        if (! $user) {
+            return null;
+        }
+        if (password_verify($password, $user->getPasswordHash())) {
+            return $user;
+        }
+        return null;
+    }
 
-        return true;
+    public function userExists(string $username): bool
+    {
+        return (bool) $this->users->findByUsername($username);
     }
 }
