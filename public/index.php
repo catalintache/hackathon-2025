9abca9887d005b868a938fb11248337915b01185
Catalin-Marian Tache <catalinmariantache@gmail.com>
2025-05-24 12:32:15 +0300

    Start session in public/index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,12 @@
 <?php
-
-declare(strict_types=1);
-
-// initializing class autoloader
-require __DIR__.'/../vendor/autoload.php';
+session_start();
+require __DIR__ . '/../vendor/autoload.php';
 
 use App\Kernel;
 use Dotenv\Dotenv;
 
-// loading .env file variables
-$dotenv = Dotenv::createImmutable(__DIR__.'/../');
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-// creating and running web application
 $app = Kernel::createApp();
 $app->run();
